bfab15b422221ca1c5d7101b4be3f6f913354208
KirillBelyaev74 <kirbel74@gmail.com>
2023-04-27 15:28:12 +0500

    log

--- src/main/kotlin/ru/logging/service/LoggingListener.kt
+++ src/main/kotlin/ru/logging/service/LoggingListener.kt
@@ -1,14 +1,10 @@
 package ru.logging.service
 
 import org.springframework.context.ApplicationListener
-import org.springframework.context.event.EventListener
-import org.springframework.stereotype.Component
 import ru.logging.model.LoggingEvent
 
-@Component
 open class LoggingListener : ApplicationListener<LoggingEvent> {
 
-    @EventListener
     override fun onApplicationEvent(event: LoggingEvent) {
         println(event.logging)
     }
